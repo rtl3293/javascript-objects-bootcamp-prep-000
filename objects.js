0f4a4354d771cbeacde17e306dd8385d23b4b030
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 16:32:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {"Kanye": "Gold Digger"}
+var playlist = {Kanye: "Gold Digger"}
 
 function updatePlaylist(playlist, artist, songTitle) {
   return Object.assign({}, playlist, {[artist]:songTitle})
