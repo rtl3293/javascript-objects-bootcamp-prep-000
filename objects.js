c25705e0daf401150524526994403bea295125d9
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 16:23:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {car: "green"}
 function updatePlaylist(playlist, artist, songTitle) {
   return Object.assign({}, playlist, {[artist]:songTitle})
 }
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist
+  return playlist
+}
